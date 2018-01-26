ed3329000e95637c6ab0694977f57826af9b0409
Deborah Akers <akersds18@gmail.com>
2018-01-26 13:37:11 -0500

    Finished VirtualPet

---
+++ src/main/java/virtualpet/VirtualPet.java
@@ -0,0 +1,43 @@
+package virtualpet;
+
+public class VirtualPet {
+
+	private int hunger = 20;
+
+	private int snack = 20;
+
+	private int play = 20;
+
+	private int bathroom = 20;
+
+	public VirtualPet(int hunger, int snack, int play, int bathroom)
+		this.hunger= hunger;
+		this.snack=snack;
+		this.play=play;
+		this.bathroom=bathroom;
+}
+
+	public int getHunger() {
+		return hunger;
+	}
+
+	public int getSnack() {
+			return snack;
+			
+	}
+	
+	public int getPlay() {
+		return play;
+	}
+		
+	public int getBathroom() {
+			return bathroom;
+			
+			
+		
+			
+		
+		
+	
+
+}
---
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -0,0 +1,35 @@
+package virtualpet;
+
+import java.util.Scanner;
+
+
+public class VirtualPetApp {
+
+
+	public static void main(String[]args) {
+		Scanner input = new Scanner(System.in);
+		
+		VirtualPet name = new VirtualPet (hunger, snack, thirst, play,bathroom);
+		String pI = "BoomBoom";
+		
+		System.out.println("Hi! I'm your new virtual pet BoomBoom");
+		String enteredPet =input.nextLine();
+		
+		while(!myAccount.allowAccess(enteredPin)) {
+		System.out.println("Invalid access. Try again.");
+		enteredPin = input.nextLine();
+		
+		System.out.println("Welcome to Bank of We Can Code IT!");
+		System.out.println("Choose an option.");
+		System.out.println("Press 1 to deposit funds.");
+		System.out.println("Press 2 to withdraw funds.");
+		System.out.println("Press 3 to check balance.");
+		System.out.println("Press 4 to exit.");
+		String optionEntered = input.nextLine();
+	
+	
+	
+}
+}
+	}
+
