c2b36418e3a168e9b752527f6992e9c2a64c02fd
Deborah Akers <akersds18@gmail.com>
2018-01-27 16:38:37 -0500

    update

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,30 +1,37 @@
 package virtualpet;
 
 public class VirtualPet {
-	
+
 	private String name = "BoomBoom";
 	private int hunger = 20;
-	
+
 	private int snack = 20;
 	private int thirst = 20;
 	private int play = 20;
 
 	private int bathroom = 20;
+
 	public VirtualPet() {
-		
+
 	}
+
 	public VirtualPet(int hunger, int snack, int thirst, int play, int bathroom) {
-		
+
 		// constructors
-		this.hunger= hunger;
-		this.snack=snack;
-		this.thirst=thirst;
-		this.play=play;
-		this.bathroom=bathroom;
-}
+		this.hunger = hunger;
+		this.snack = snack;
+		this.thirst = thirst;
+		this.play = play;
+		this.bathroom = bathroom;
+	}
 
 	public int getHunger() {
 		return hunger;
+	}
+
+	public int getThirst() {
+		return thirst;
+
 	}
 
 	public int getSnack() {
@@ -37,26 +44,54 @@ public class VirtualPet {
 	}
 
 	public int getBathroom() {
-			return bathroom;
-			
+		return bathroom;
+
 	}
-	//class behavior methods
+
+	public String getName() {
+		return name;
+	}
+
+	// class behavior methods
 	public void tick() {
 		this.hunger++;
 		this.snack++;
 		this.thirst++;
 		this.play++;
 		this.bathroom++;
-	
+
 	}
+
+	public void feedPet() {
+
+	}
+
+	public void snackPet() {
+
+	}
+
+	public void waterPet() {
+
+	}
+
+	public void bathroomPet() {
+
+	}
+
+	public void playPet() {
+
+	}
+
 	public String displayStatus() {
-		return petName + "\nHunger: " + Hunger + "\nSnack: " + snack + "\nThirst:" + Thirst
-				+ "\nPlay" + Play + "\nBathroom: " + Bathroom;
-		
+
+		String petName = null;
+		return petName + "\nHunger: " + hunger + "\nSnack: " + snack + "\nThirst:" + thirst + "\nPlay" + play
+				+ "\nBathroom: " + bathroom;
+
 	}
-	
+
 	{
-	
+
 	}
 
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -2,27 +2,69 @@ package virtualpet;
 
 import java.util.Scanner;
 
-
 public class VirtualPetApp {
 
-
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		VirtualPet pet = new VirtualPet ();
-		
-		System.out.println(pet.equals(pet.displayStatus()));
-		System.out.println();
-		System.out.println("What do you want to do offer BoomBoom?");
-		System.out.println("Press 1 for food.");
-		System.out.println("Press 2 for snack.");
-		System.out.println("Press 3 for soda.");
-		System.out.println("Press 4 for a ball.");
-		System.out.println("Press 5 for the restroom");
-		String optionEntered = input.nextLine();
-	
-	
-	
-}
-}
-	
 
+		VirtualPet pet = new VirtualPet();
+
+		// String decision = "";
+		// while (decision.equalsIgnoreCase("Yes")) {
+
+		System.out.println("What do you want to name your pet?");
+		String enterName = input.nextLine();
+
+		String optionsEntered = "";
+		do {
+			System.out.println("Hunger: " + pet.getHunger());
+			System.out.println("Snack: " + pet.getSnack());
+			System.out.println("Thirst: " + pet.getThirst());
+			System.out.println("Play: " + pet.getPlay());
+			System.out.println("Bathroom: " + pet.getBathroom());
+			System.out.println();
+			System.out.println("Press 1 to feed " + enterName);
+			System.out.println("Press 2 to give a snack to " + enterName);
+			System.out.println("Press 3 to give " + enterName + " water");
+			System.out.println("Press 4 to play with " + enterName);
+			System.out.println("Press 5 to take " + enterName + " to bathroom");
+			optionsEntered = input.nextLine();
+
+			// if(optionsEntered.equals("1")) {
+			// System.out.println("Enter an amount to feed your pet");
+			// int foodAmount = input.nextInt();
+			// System.out.println("");
+			// System.out.println("You have fed your ");
+			//
+			switch (optionsEntered) {
+			case "1": {
+				pet.feedPet();
+				System.out.println("PLease feed " + pet.getName() + " . ");
+				break;
+			}
+			case "2": {
+				pet.snackPet();
+				System.out.println("PLease feed " + pet.getName() + " . ");
+				break;
+			}
+			case "3": {
+				pet.waterPet();
+				System.out.println("Please give " + pet.getName() + " . ");
+				break;
+			}
+			case "4": {
+				pet.playPet();
+				System.out.println("Please play " + pet.getName() + " . ");
+				break;
+			}
+			case "5": {
+				pet.bathroomPet();
+				System.out.println("Please take " + pet.getName() + " . ");
+				break;
+			}
+
+			}
+		} while (true);
+
+	}
+}
